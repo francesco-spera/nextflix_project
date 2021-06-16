bae633c288cabd7ac2e5bb0b302f3f5e3c5ecf6b
nunzia-elefante <37613409+nunzia-elefante@users.noreply.github.com>
2021-06-16 22:01:45 +0200

    update querys.py

--- netflix_project/querys.py
+++ netflix_project/querys.py
@@ -1,7 +1,8 @@
 from sqlite3 import DatabaseError
 from flask import logging
 import connection_pool as cp
-		
+
+
 # searching query
 
 def find_all():
@@ -60,20 +61,21 @@ def search_by_title_type(title, type):
 		logging.warning('DB exception: %s' % e)
 	return results
 
-def search_by_type_genre(type, genre):
+def search_by_type_duraton(type, duration):
 	try:
 		con = cp.connection_pool()
-		results = con.find({'$and':[{'type': type}, {'listed_in': genre}]})
+		results = con.find({'$and':[{'type': type}, {'duration': duration}]})
 	except DatabaseError as e:
 		logging.warning('DB exception: %s' % e)
 	return results
+
 
 # counting query
 
 def count_by_genre(genre):
 	try:
 		con = cp.connection_pool()
-		results = con.count({'listed_in': genre})
+		results = len(list(con.find({'listed_in': genre})))
 	except DatabaseError as e:
 		logging.warning('DB exception: %s' % e)
 	return results
@@ -81,37 +83,38 @@ def count_by_genre(genre):
 def count_by_duration(duration):
 	try:
 		con = cp.connection_pool()
-		results = con.count({'duration': duration})
+		results = len(list(con.find({'duration': duration})))
 	except DatabaseError as e:
 		logging.warning('DB exception: %s' % e)
 	return results
 
 def count_by_type():
-	counters = {}
+	results = []
 	try:
 		con = cp.connection_pool()
-		results = con.count({'type': 'Movie'})
-		counters['Movie'] = results
-		results = con.count({'type': 'TV Show'})
-		counters['TV Show'] = results
+		results_movie = len(list(con.find({'type': 'Movie'})))
+		#counters['Movie'] = results
+		results_show = len(list(con.find({'type': 'TV Show'})))
+		#counters['TV Show'] = results
+		results = [results_movie, results_show]
 	except DatabaseError as e:
 		logging.warning('DB exception: %s' % e)
-	return counters
+	return results
 
 def count_by_rating():
 	counters = {}
 	try:
 		con = cp.connection_pool()
-		results = con.count({'rating': 'TV-MA'})
+		results = len(list(con.find({'rating': 'TV-MA'})))
 		counters['TV-MA'] = results
-		results = con.count({'rating': 'TV-14'})
+		results = len(list(con.find({'rating': 'TV-14'})))
 		counters['TV-14'] = results
-		results = con.count({'rating': 'TV-PG'})
+		results = len(list(con.find({'rating': 'TV-PG'})))
 		counters['TV-PG'] = results
-		results = con.count({'rating': 'R'})
+		results = len(list(con.find({'rating': 'R'})))
 		counters['R'] = results
-		results = con.count({'rating': 'PG-13'})
+		results = len(list(con.find({'rating': 'PG-13'})))
 		counters['PG-13'] = results
 	except DatabaseError as e:
 		logging.warning('DB exception: %s' % e)
-	return counters
+	return results
