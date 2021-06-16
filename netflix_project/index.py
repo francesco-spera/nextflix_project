d0fec8992dadbc899c23d849ca804900711b61c7
nunzia-elefante <37613409+nunzia-elefante@users.noreply.github.com>
2021-06-16 16:55:45 +0200

    update index.py

--- netflix_project/index.py
+++ netflix_project/index.py
@@ -1,13 +1,20 @@
 from flask import Flask, redirect, render_template, url_for, request
+import connection_pool as cp
 import querys as q
 
-app = Flask(__name__)
+app = cp.app
 
 
 @app.route('/')
 def index():
 	print(q.search_by_title('9'))
 	return render_template('index.html')
+
+@app.route('/search.html', methods=['POST'])
+def search():
+	tag_name = request.form.get('tag_name')
+	info = q.search_by_title(tag_name)
+	return render_template('search.html', info=info)
 
 
 if __name__ == '__main__':
