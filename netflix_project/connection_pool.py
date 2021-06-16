dd3382ddb084f3c16d3c27fc2cce2b3d9ed274d6
nunzia-elefante <37613409+nunzia-elefante@users.noreply.github.com>
2021-06-16 16:52:27 +0200

    update connection_pool.py

--- netflix_project/connection_pool.py
+++ netflix_project/connection_pool.py
@@ -1,5 +1,10 @@
-import pymongo
+from flask_pymongo import PyMongo
+from flask import Flask
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://Nunzia:freanat8@pythoncluster.ldlif.mongodb.net/quickstart?authSource=admin&replicaSet=atlas-d5hj2e-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
+mongo = PyMongo(app)
 
 def connection_pool():
-	client = pymongo.MongoClient("mongodb+srv://Nunzia:freanat8@pythoncluster.ldlif.mongodb.net/quickstart?authSource=admin&replicaSet=atlas-d5hj2e-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true")
-	return client.db.netflix
+	netflix = mongo.db.netflix
+	return netflix		
