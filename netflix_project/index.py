ecbbcb938614ebcff4a6f8206e133fed32b22fea
nunzia-elefante <37613409+nunzia-elefante@users.noreply.github.com>
2021-06-16 22:00:44 +0200

    update index.py

--- netflix_project/index.py
+++ netflix_project/index.py
@@ -1,33 +1,42 @@
-from flask import Flask, redirect, render_template, url_for, request
+from flask import render_template, request
+import connection_pool as cp
 import querys as q
 
-app = Flask(__name__)
+app = cp.app
+
+colors = [
+    "#E74C3C", "#F1C40F", "#7FB3D5", "#C0392B",
+    "#ABCDEF", "#F5B041", "#52BE80", "#AAB7B8"]
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	titles = q.find_all()
+	return render_template('index.html', titles=titles)
 
-@app.route('/search', methods=['POST', 'GET'])
+@app.route('/search.html', methods=['POST'])
 def search():
 	tag_name = request.form.get('tag_name')
 	info = q.search_by_title(tag_name)
 	return render_template('search.html', info=info)
 
-@app.route('/graphic_type', methods=['POST', 'GET'])
-def stats_view_type():
-	labels = ['MOVIE', 'TV SHOW']
-	values = list(q.count_by_type())
-	colors = ['#FFFF00', '#FFA500']
+@app.route("/search_advanced.html")
+def search_advanced():
+    	return render_template('search_advanced.html')
+
+@app.route("/search_advanced.html", methods=['POST'])
+def search_advanced_2():
+    	tag_show = request.form.get('show')
+    	tag_time = request.form.get('time')
+    	titles = list(q.search_by_type_duraton(tag_show, tag_time))
+    	return render_template('search_advanced.html', titles=titles)
+
+@app.route("/graphic_type.html")
+def graphic_type():
+	labels = ['Movie', 'TV Show']
+	values = q.count_by_type()
 	return render_template('graphic_type.html', set=zip(values, labels, colors))
-
-@app.route('/graphic_genre', methods=['POST', 'GET'])
-def stats_view_genre():
-	return render_template('graphic_genre.html')
-
-@app.route('/graphic_rating', methods=['POST', 'GET'])
-def stats_view_rating():
-	return render_template('graphic_rating.html')
 
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
